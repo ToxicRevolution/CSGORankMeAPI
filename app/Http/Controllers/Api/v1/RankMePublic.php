0781891b9beb3027671789cafd8c9521106d1983
kennyLtv <kenny@kennyl.tv>
2017-07-13 16:24:50 -0400

    added sorting to the /players endpoint

--- app/Http/Controllers/Api/v1/RankMePublic.php
+++ app/Http/Controllers/Api/v1/RankMePublic.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\RankMeAPI;
-use Illuminate\Http\Request;
+use Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-
+use Input;
 
 
 class RankMePublic extends Controller
@@ -25,14 +25,25 @@ class RankMePublic extends Controller
 
     public function listAllPaginate(RankMeAPI $rankMeAPI)
     {
-        $info = RankMeAPI::paginate(15);
+        $info = RankMeAPI::orderBy('score', 'desc')->paginate(15);
         return response()->json($info,200,[],JSON_PRETTY_PRINT);
     }
 
-    public function listAllPlayers(RankMeAPI $rankMeAPI)
+    public function listAllPlayers(RankMeAPI $rankMeAPI, Request $request)
     {
       $info = RankMeAPI::select('id', 'name', 'steam', 'kills', 'deaths')->get();
-      return response()->json($info,200,[],JSON_PRETTY_PRINT);
+      // Switch statement to deal with sorting of data based of query string
+      switch (Request::get('sort')){
+        case '':
+          return response()->json($info,200,[],JSON_PRETTY_PRINT);
+        case 'desc':
+          $info = RankMeAPI::orderBy('score', 'desc')->get();
+          return response()->json($info,200,[],JSON_PRETTY_PRINT);
+        case 'asc':
+          $info = RankMeAPI::orderBy('score', 'asc')->get();
+          return response()->json($info,200,[],JSON_PRETTY_PRINT);
+        break;
+      }
     }
 
     public function listPlayer(RankMeAPI $rankMeAPI, $id)
